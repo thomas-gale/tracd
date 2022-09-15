bc0c9e4c5dcf42acdeaeee6e22ea77614a7d0827
Thomas Gale <gale.thomasjames@gmail.com>
2022-09-15 22:52:29 +0100

    Experimenting with custom geometry.

--- src/components/viewport/MapBoxMap.tsx
+++ src/components/viewport/MapBoxMap.tsx
@@ -1,8 +1,9 @@
 // Attribution: https://dev.to/dqunbp/using-mapbox-gl-in-react-with-next-js-2glg
+import * as THREE from "three";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useRef, useState } from "react";
-// import the mapbox-gl styles so that the map is displayed correctly
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export const MapBoxMap = () => {
   // this is where the map instance will be stored after initialization
@@ -31,13 +32,116 @@ export const MapBoxMap = () => {
     // Look at Brum ;)
     const camera = mapboxMap.getFreeCameraOptions();
     camera.position = mapboxgl.MercatorCoordinate.fromLngLat(
-      [-1.898575, 50], // LngLat
-      500000 // Altitude
+      [-1.898575, 52.489471], // LngLat
+      1000 // Altitude
     );
     camera.lookAtPoint([-1.898575, 52.489471]);
     mapboxMap.setFreeCameraOptions(camera);
 
-    // save the map object to React.useState
+    // Render custom geometry
+    // parameters to ensure the model is georeferenced correctly on the map
+    const modelRotate = [Math.PI / 2, 0, 0];
+
+    const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+      [-1.898575, 52.489471], // LngLat
+      0 //Altitude
+    );
+
+    // transformation parameters to position, rotate and scale the 3D model onto the map
+    const modelTransform = {
+      translateX: modelAsMercatorCoordinate.x,
+      translateY: modelAsMercatorCoordinate.y,
+      translateZ: modelAsMercatorCoordinate.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      /* Since the 3D model is in real world meters, a scale transform needs to be
+       * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+       */
+      scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+    };
+
+    // configuration of the custom layer for a 3D model per the CustomLayerInterface
+    const customLayer = {
+      id: "3d-model",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map: any, gl: any) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        // create two three.js lights to illuminate the model
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        const loader = new GLTFLoader();
+        loader.load(
+          "https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf",
+          (gltf: any) => {
+            this.scene.add(gltf.scene);
+          }
+        );
+        this.map = map;
+
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        });
+
+        this.renderer.autoClear = false;
+      },
+      render: function (gl: any, matrix: any) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(1, 0, 0),
+          modelTransform.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 1, 0),
+          modelTransform.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 0, 1),
+          modelTransform.rotateZ
+        );
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+          .makeTranslation(
+            modelTransform.translateX,
+            modelTransform.translateY,
+            modelTransform.translateZ as number
+          )
+          .scale(
+            new THREE.Vector3(
+              modelTransform.scale,
+              -modelTransform.scale,
+              modelTransform.scale
+            )
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      },
+    } as any;
+
+    mapboxMap.on("style.load", () => {
+      mapboxMap.addLayer(customLayer, "waterway-label");
+    });
+
+    // Finally, save the map object to React.useState
     setMap(mapboxMap);
 
     return () => {
