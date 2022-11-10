dbd6bf148ce0e85427e3993cd2dcab4de28044b1
Thomas Gale <gale.thomasjames@gmail.com>
2022-11-10 22:25:09 +0000

    Switching from dynamic routes to query (as static builds don't support the former).

--- src/pages/matter/southern-golddigger.tsx
+++ src/pages/matter/southern-golddigger.tsx
@@ -1,24 +1,22 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 
 // A two step component that allows the user to first select an IPFS storage provider and then allows them to supply the hash to the webapp.
 const SouthernGolddigger = (): JSX.Element => {
   const router = useRouter();
   const { id } = router.query;
-
-  // useEffect(() => {
-  //   const id_num = id as unknown as number;
-  //   if (id_num < 0 || id_num > 24) {
-  //     router.push("/404");
-  //   }
-  // }, [id, router]);
+  const idn = useMemo(() => id as unknown as number, [id]);
 
   return (
     <div className="h-full flex flex-col p-4 space-y-4">
       <div className="flex flex-col h-full p-4 space-y-2 rounded-xl bg-dark">
-        <h2 className="text-light">
-          Information for bottle: <b>{id}</b>
-        </h2>
+        {idn === undefined || idn < 0 || idn > 24 ? (
+          <h2 className="bg-red-400 text-light">Unrecognized bottle</h2>
+        ) : (
+          <h2 className="text-light">
+            Information for bottle: <b>{idn}</b>
+          </h2>
+        )}
       </div>
     </div>
   );
