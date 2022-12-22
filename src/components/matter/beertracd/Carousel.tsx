d563ee05e9d41ddb3ced676fe7caf59764f3f0b6
Thomas Gale <gale.thomasjames@gmail.com>
2022-12-22 11:46:51 +0000

    Object contain.

--- src/components/matter/beertracd/Carousel.tsx
+++ src/components/matter/beertracd/Carousel.tsx
@@ -8,7 +8,7 @@ export interface CarouselProps {
 export const Carousel = ({ uniqueId, images }: CarouselProps) => {
   const len = useMemo(() => images.length, [images]);
   return (
-    <div className="carousel w-full rounded-lg">
+    <div className="carousel w-full">
       {images.map((image, index) => (
         <div
           id={`slide-${uniqueId}-${index}`}
@@ -16,7 +16,7 @@ export const Carousel = ({ uniqueId, images }: CarouselProps) => {
           className="carousel-item relative w-full"
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={image} alt="carousel image" className="w-full" />
+          <img src={image} alt="carousel image" className="object-contain"/>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a
               href={`#slide-${uniqueId}-${(len + index - 1) % len}`}
